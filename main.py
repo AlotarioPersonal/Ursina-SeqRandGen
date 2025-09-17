e79947dec6adf17236962a7a840447027846187f
Alot <126506217+AlotarioPersonal@users.noreply.github.com>
2025-09-17 12:15:11 -0500

    patch va4

--- main.py
+++ main.py
@@ -2,13 +2,32 @@
 #
 #   2D List-based Dimensional Exploration
 #   L. Kaminski
-#   Version 0.2, 2024-11-10
+#   Version 0.4, 2025-09-17
+#
+#   Exploration mock-raycast engine written in Python using URSINA.
+#   Written in five days.
+#   
 #
 #   REQUIRED PACKAGES:
 #   -ursina/dependencies
 #   -threading
+#   
+#   V 0.4 PATCH:
+#   -fixed a bug where the combination loop for the items just. didn't work. replaced with a less garbage method
+#   -allow for the saving of 3d meshes of generated dungeons to a .ursinamesh file, which you can convert to obj using my very cool script
+#   -disabled collision and opted for a gravity-less system with no collision AT ALL, because ursina's mesh collider calculations are unoptimized and stupid and make me want to head explode
+#   -removed obsolete wall_ent entity because it was literally a carbon copy of the floor_ent
 #
 ######################################
+
+import os
+
+if os.name=="nt":
+    os.system('cls')
+else:
+    os.system('clear')
+
+confirmation = input("Would you like to save the model for this generation? It will be saved as a .ursinamesh and you will have to convert it with my retarded script, so please consider this seriously? [y/n]")
 
 from ursina import * #get ursina library
 from maps import level_data #level data (see maps/level_data.py)
@@ -16,39 +35,25 @@ from ursina.prefabs.first_person_controller import FirstPersonController
 from ursina.shaders import basic_lighting_shader
 import threading
 
-maplevelname = level_data.level_data_rand_big
+maplevelname = level_data.level_data_rand_continent
 
 t_ev = threading.Event()
 app = Ursina() #define app
 Entity.geom=False
 from maps.entity_data import floor_ent
-from maps.entity_data import wall
 floor_ent.geom=True
-wall.geom=True
 
-
-window.editor_ui.disable()
-camera.ui.disable()
-
-
-
-
-camera.clip_plane_far=75
 
 
 skybox = Sky() #initialize skybox
 skybox.texture="skybox.png"
 total = Entity(model=None, collider=None)
-#skybox.color=color.black
-#skybox.position=player.position
-scene.fog_density=0.07
-scene.fog_color=color.black
+
 camera.fov = 110
 skybox.eternal=True
 floor_ent.eternal=True
-wall.eternal=True
+total.eternal=True
 floor_ent.position=Vec3(9999999,9999999,99999999)
-wall.position=Vec3(9999999,9999999,99999999)
 
 last_indexed_ent = 0
 
@@ -58,20 +63,32 @@ last_indexed_ent = 0
 
 def resetScene():
     global total
-    player.position=Vec3(0,0,0)
+    
     scene.clear()
+    for a in scene.collidables:
+        if a.geom!=True:
+            if a==FirstPersonController:
+                pass
+            else:
+                a.disable()
     total = Entity(model=None, collider=None)
     player.position=Vec3(0,0,0)
-
 
 def input(key):
     global player_cam_speed
     if key == 'c': #debugging feature
         print(player.world_position)
+        player.gravity=-0.4
+    elif key == 'h':
+        player.gravity=-0.5
+    else:
+        player.gravity=0.0
     
     if key == 'r': #debugging feature
+        
         resetScene()
         gen_level(maplevelname)
+        
     
     if key == 'escape': #kill world
         quit()
@@ -81,7 +98,6 @@ def input(key):
 
 def gen_level(map_dat): #generate level from 2D list. this is where the fun begins
     global floor_ent
-    global wall
     global last_indexed_ent
     x_incrementer = 0 #x position tracker
     z_incrementer = 0 #z position tracker
@@ -94,25 +110,26 @@ def gen_level(map_dat): #generate level from 2D list. this is where the fun begi
                 x_incrementer += 1 #the next block is 1 over from this item in the row (loads from left to right)
                 indexer += 1 #tracks where you are in the row, so this needs to be incremented
                 last_indexed_ent=0
-                #floor_ent.combine()
             elif item == 1: #otherwise, it's a wall, make it a wall
-                wall_dup = duplicate(wall, position=Vec3(x_incrementer*10, -5, z_incrementer*10))
+                wall_dup = duplicate(floor_ent, position=Vec3(x_incrementer*10, -5, z_incrementer*10))
                 wall_dup.eternal=False
                 x_incrementer += 1
                 indexer += 1
                 last_indexed_ent=1
-                #wall.combine()
+                wall_dup.color=color.white
             elif item == 2: #random generation function
                 random_tile = random.randrange(0,100)
+                set_chance_wall = 25
                 
-                if random_tile<=25:
-                    wall_dup = duplicate(wall, position=Vec3(x_incrementer*10, -5, z_incrementer*10))
+                if random_tile<=set_chance_wall:
+                    wall_dup = duplicate(floor_ent, position=Vec3(x_incrementer*10, -5, z_incrementer*10), parent=total)
                     wall_dup.eternal=False
-                    #wall.combine()
-                elif random_tile>=25:
-                    floor_dup = duplicate(floor_ent, position=Vec3(x_incrementer*10, -10, z_incrementer*10))
+                    wall_dup.color=color.white
+                    
+                elif random_tile>=set_chance_wall:
+                    floor_dup = duplicate(floor_ent, position=Vec3(x_incrementer*10, -10, z_incrementer*10), parent=total)
                     floor_dup.eternal=False
-                    #floor_ent.combine()
+                    
                     
                 
                 x_incrementer += 1
@@ -125,31 +142,14 @@ def gen_level(map_dat): #generate level from 2D list. this is where the fun begi
                 x_incrementer = 0 #reset the x position
                 z_incrementer += 1 #move (physically) down one row
             print("generating...")
-
-    for x in scene.entities:
-        print("GEORGE BUSH, COLUMBINE")
-        if x != wall and x != floor_ent and x!= total and x!=skybox:
-            x.parent=total
-            print("TERROR ON THE AIR LINE")
-    
-
+    total.combine()
 
         
 
 t1=threading.Thread(target=gen_level(maplevelname))
 t1.start()
 
-def distanceCheckFar():
-    for Entity in scene.entities:
-        if distance_xz(player, Entity) >= 25:
-            if Entity.enabled==True and Entity != player:
-                Entity.disable()
-        else:
-            if Entity.enabled==False and Entity != player:
-                Entity.enable()
-
 def update():
-    #distanceCheckFar()
     #doom/wolfenstein style turning
     if held_keys['left arrow']: #rotate left
         player.rotation_y -= player_cam_speed
@@ -170,19 +170,39 @@ input_handler.unbind('a')
 input_handler.unbind('s')
 input_handler.unbind('d')
 player.eternal=True
-player.gravity = 0.98 #rate of which player character falls
+player.gravity = 0.0 #rate of which player character falls
 player.speed = 13
 
-total.combine()
-total.collider="mesh"
-total.texture="brick"
-total.ignore=True
-print('I CANT TAKE IT ANYMOOOOOORE')
 
-print("BEGIN SCENE ENTITIES")
-for h in scene.collidables:
-    print(h)
-    print("\n")
-print("END SCENE ENTITIES")
+
+
+
+Entity.color="red"
+
+
+
+
+
+
+
+scene.clear()
+
+total.texture='brick'
+from maps.entity_data import floor_ent
+
+for z in total.children: #debug
+    print(str(z) + " TOTAL-CHILD")
+
+for y in scene.entities: #debug
+    print(str(y.collider) + " SCENE ENTITY")
+
+if confirmation=='y':
+    varname = random.randint(1, 9999999)
+    fullname = str("dungeon_" + str(varname))
+    total.model.save(name=fullname, folder= Path("models_compressed"))
+
+
+scene.fog_density=0.07
+scene.fog_color=color.black
 
 app.run() #execute the application
