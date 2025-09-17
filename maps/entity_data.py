b00b9c59be61981472175a2f0140eabbb6a25736
Alot <126506217+AlotarioPersonal@users.noreply.github.com>
2025-09-17 13:49:10 -0500

    removed obsolete entities

--- maps/entity_data.py
+++ maps/entity_data.py
@@ -3,5 +3,4 @@ from ursina.shaders import basic_lighting_shader
 
 
 floor_ent = Entity(model="cube", scale=10, ignore=True, collider='mesh', texture='brick', color=color.brown, shader=basic_lighting_shader)
-wall = Entity(model="cube", scale=10, ignore=True, collider='mesh', texture='brick', shader=basic_lighting_shader)
 
