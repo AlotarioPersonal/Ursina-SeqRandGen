48e81768daa0a2856fcd678d913aa616e089c01d
Alot <126506217+AlotarioPersonal@users.noreply.github.com>
2025-10-08 13:11:01 -0500

    survey for CPU power, disable collision if you pass a predefined render limit-- good for computers with not a lot of processing power

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 #
 #   2D List-based Dimensional Exploration
 #   L. Kaminski
-#   Version 0.4, 2025-09-17
+#   Version 0.5, 2025-10-08
 #
 #   Exploration mock-raycast engine written in Python using URSINA.
 #   Written in five days.
@@ -35,7 +35,13 @@ from ursina.prefabs.first_person_controller import FirstPersonController
 from ursina.shaders import basic_lighting_shader
 import threading
 
-maplevelname = level_data.level_data_rand_continent
+maplevelname = level_data.level_data_rand_custom
+
+if maplevelname == level_data.level_data_rand_custom:
+    x_len = int(input("Enter the x-length of your generated world: "))
+    y_len = int(input("Enter the y-length of your generated world: "))
+    for i in range(x_len):
+        level_data.level_data_rand_custom.append([2] * y_len)
 
 t_ev = threading.Event()
 app = Ursina() #define app
@@ -43,6 +49,8 @@ Entity.geom=False
 from maps.entity_data import floor_ent
 floor_ent.geom=True
 
+survey_cpu_limit = int(input("On a scale of 1-10, how powerful is your computer? 1 being shit, 10 being the best: "))
+max_render_limit = survey_cpu_limit * 10
 
 
 skybox = Sky() #initialize skybox
@@ -57,6 +65,7 @@ floor_ent.position=Vec3(9999999,9999999,99999999)
 
 last_indexed_ent = 0
 
+cycles = 0
 
 #define entity types
 
@@ -78,11 +87,6 @@ def input(key):
     global player_cam_speed
     if key == 'c': #debugging feature
         print(player.world_position)
-        player.gravity=-0.4
-    elif key == 'h':
-        player.gravity=-0.5
-    else:
-        player.gravity=0.0
     
     if key == 'r': #debugging feature
         
@@ -99,6 +103,7 @@ def input(key):
 def gen_level(map_dat): #generate level from 2D list. this is where the fun begins
     global floor_ent
     global last_indexed_ent
+    global cycles
     x_incrementer = 0 #x position tracker
     z_incrementer = 0 #z position tracker
     indexer = 0 #index tracker (the actual index() function doesn't do what i need it to in this scenario because the list has only 2 datatypes)
@@ -110,12 +115,14 @@ def gen_level(map_dat): #generate level from 2D list. this is where the fun begi
                 x_incrementer += 1 #the next block is 1 over from this item in the row (loads from left to right)
                 indexer += 1 #tracks where you are in the row, so this needs to be incremented
                 last_indexed_ent=0
+                cycles += 1
             elif item == 1: #otherwise, it's a wall, make it a wall
                 wall_dup = duplicate(floor_ent, position=Vec3(x_incrementer*10, -5, z_incrementer*10))
                 wall_dup.eternal=False
                 x_incrementer += 1
                 indexer += 1
                 last_indexed_ent=1
+                cycles += 1
                 wall_dup.color=color.white
             elif item == 2: #random generation function
                 random_tile = random.randrange(0,100)
@@ -125,11 +132,11 @@ def gen_level(map_dat): #generate level from 2D list. this is where the fun begi
                     wall_dup = duplicate(floor_ent, position=Vec3(x_incrementer*10, -5, z_incrementer*10), parent=total)
                     wall_dup.eternal=False
                     wall_dup.color=color.white
-                    
+                    cycles += 1
                 elif random_tile>=set_chance_wall:
                     floor_dup = duplicate(floor_ent, position=Vec3(x_incrementer*10, -10, z_incrementer*10), parent=total)
                     floor_dup.eternal=False
-                    
+                    cycles += 1
                     
                 
                 x_incrementer += 1
@@ -141,7 +148,8 @@ def gen_level(map_dat): #generate level from 2D list. this is where the fun begi
                 indexer = 0 #reset the index tracker for the next row
                 x_incrementer = 0 #reset the x position
                 z_incrementer += 1 #move (physically) down one row
-            print("generating...")
+                cycles += 1
+            print("Generating level... " + str(cycles) + " cycles have passed")
     total.combine()
 
         
@@ -157,11 +165,17 @@ def update():
     if held_keys['right arrow']: #rotate right
         player.rotation_y += player_cam_speed
 
+def renderCheck():
+    if x_len >= max_render_limit or y_len >= max_render_limit:
+        total.collider=None
+        player.gravity=0.0
+
+
 player = FirstPersonController() #base ursina first person controller
-player.position=(0,0,0) #set pos
+player.position=(0,1,0) #set pos
 player.mouse_sensitivity = Vec2(0, 0) #kill the mouse
 player.cursor.color = color.red #turns the cursor gray hur dur fuck face
-player.jump_height = 0 #kills jumping
+player.jump_height = 6 #kills jumping
 player_cam_speed = 2 #turn speed for the camera, left and right arrow keys
 input_handler.bind('up arrow','w') #bind arrow keys forward and backward to movement, doom-style
 input_handler.bind('down arrow','s')
@@ -170,19 +184,12 @@ input_handler.unbind('a')
 input_handler.unbind('s')
 input_handler.unbind('d')
 player.eternal=True
-player.gravity = 0.0 #rate of which player character falls
+player.gravity = 0.50 #rate of which player character falls
 player.speed = 13
 
 
-
-
-
+total.collider='mesh'
 Entity.color="red"
-
-
-
-
-
 
 
 scene.clear()
@@ -201,6 +208,7 @@ if confirmation=='y':
     fullname = str("dungeon_" + str(varname))
     total.model.save(name=fullname, folder= Path("models_compressed"))
 
+renderCheck()
 
 scene.fog_density=0.07
 scene.fog_color=color.black
