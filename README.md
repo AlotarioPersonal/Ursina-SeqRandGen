b04663442d11527515db0a0cd1403fb9c6f3c6e6
Alot <126506217+AlotarioPersonal@users.noreply.github.com>
2025-11-23 14:10:12 -0600

    patch 0.5

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 3D MAZE GENERATION
 
-![image](https://github.com/user-attachments/assets/35a1c83d-cedd-4eb9-b7e5-bc82d68c8ce7)
+
 
 
 Contained in level-data.py is a repository of 2-dimensional levels. They can have three corresponding values in these lists--
@@ -17,6 +17,6 @@ Contained in level-data.py is a repository of 2-dimensional levels. They can hav
 
 Used this framework to create a game called Axe Quest. You can find it here: https://brimlessentertainment.itch.io/axe-quest-1994
 
-As far as the random generation goes, right now it's mostly completely random. I don't have a way to index the random 2D data to a list and adjust accordingly to make a fully traversable set of terrain, but what is traversable (somewhere around 1,000 objs) has been heavily optimized.
+As far as the random generation goes, right now it's mostly completely random. I don't have a way to index the random 2D data to a list and adjust accordingly to make a fully traversable set of terrain.
 
 You may use this in your projects, I don't care
--- main.py
+++ main.py
@@ -1,24 +1,25 @@
-######################################
-#
-#   2D List-based Dimensional Exploration
-#   L. Kaminski
-#   Version 0.5, 2025-10-08
-#
-#   Exploration mock-raycast engine written in Python using URSINA.
-#   Written in five days.
-#   
-#
-#   REQUIRED PACKAGES:
-#   -ursina/dependencies
-#   -threading
-#   
-#   V 0.4 PATCH:
-#   -fixed a bug where the combination loop for the items just. didn't work. replaced with a less garbage method
-#   -allow for the saving of 3d meshes of generated dungeons to a .ursinamesh file, which you can convert to obj using my very cool script
-#   -disabled collision and opted for a gravity-less system with no collision AT ALL, because ursina's mesh collider calculations are unoptimized and stupid and make me want to head explode
-#   -removed obsolete wall_ent entity because it was literally a carbon copy of the floor_ent
-#
-######################################
+'''
+
+   2D List-based Dimensional Exploration
+
+   Version 0.5, 2025-11-23
+
+   Exploration mock-raycast engine written in Python using URSINA.
+   Written in five days, expanded upon over the course of around a year.
+   
+
+   REQUIRED PACKAGES:
+   -ursina/dependencies
+   -threading
+   -tkinter
+   -random
+   
+   V 0.5 PATCH:
+   -ursina 8.0 optimizations automagically applied, but still need collider optimization work
+   -tkinter simple dialog window, we're officially out of terminal requirement
+   -height randomization for the buildings
+
+'''
 
 import os
 
@@ -27,19 +28,23 @@ if os.name=="nt":
 else:
     os.system('clear')
 
-confirmation = input("Would you like to save the model for this generation? It will be saved as a .ursinamesh and you will have to convert it with my retarded script, so please consider this seriously? [y/n]")
+
 
 from ursina import * #get ursina library
 from maps import level_data #level data (see maps/level_data.py)
 from ursina.prefabs.first_person_controller import FirstPersonController
-from ursina.shaders import basic_lighting_shader
+from tkinter import simpledialog
 import threading
-
+import random
 maplevelname = level_data.level_data_rand_custom
+max_height = 0
 
 if maplevelname == level_data.level_data_rand_custom:
-    x_len = int(input("Enter the x-length of your generated world: "))
-    y_len = int(input("Enter the y-length of your generated world: "))
+    global confirmation
+    confirmation = simpledialog.askstring("Input", "Would you like to save the model for this generation? [y/n]")
+    x_len = simpledialog.askinteger("Input", "Enter the x-length of your generated world: ")
+    y_len = simpledialog.askinteger("Input", "Enter the y-length of your generated world: ")
+    max_height = simpledialog.askinteger("Input", "Enter the max building height of your generated world (Max 50): ")
     for i in range(x_len):
         level_data.level_data_rand_custom.append([2] * y_len)
 
@@ -49,8 +54,9 @@ Entity.geom=False
 from maps.entity_data import floor_ent
 floor_ent.geom=True
 
-survey_cpu_limit = int(input("On a scale of 1-10, how powerful is your computer? 1 being shit, 10 being the best: "))
-max_render_limit = survey_cpu_limit * 10
+
+
+
 
 
 skybox = Sky() #initialize skybox
@@ -66,8 +72,6 @@ floor_ent.position=Vec3(9999999,9999999,99999999)
 last_indexed_ent = 0
 
 cycles = 0
-
-#define entity types
 
 
 def resetScene():
@@ -87,11 +91,6 @@ def input(key):
     global player_cam_speed
     if key == 'c': #debugging feature
         print(player.world_position)
-    
-    if key == 'r': #debugging feature
-        
-        resetScene()
-        gen_level(maplevelname)
         
     
     if key == 'escape': #kill world
@@ -104,20 +103,21 @@ def gen_level(map_dat): #generate level from 2D list. this is where the fun begi
     global floor_ent
     global last_indexed_ent
     global cycles
+    set_chance_height = 0
     x_incrementer = 0 #x position tracker
     z_incrementer = 0 #z position tracker
     indexer = 0 #index tracker (the actual index() function doesn't do what i need it to in this scenario because the list has only 2 datatypes)
     for row in map_dat: #row for indexer
         for item in row: #column for indexer
             if item == 0: #if the item is a floor, make it a floor
-                floor_dup = duplicate(floor_ent, position=Vec3(x_incrementer*10, -10, z_incrementer*10))
+                floor_dup = duplicate(floor_ent, position=Vec3(x_incrementer*10, -10, z_incrementer*10), parent=total)
                 floor_dup.eternal=False
                 x_incrementer += 1 #the next block is 1 over from this item in the row (loads from left to right)
                 indexer += 1 #tracks where you are in the row, so this needs to be incremented
                 last_indexed_ent=0
                 cycles += 1
             elif item == 1: #otherwise, it's a wall, make it a wall
-                wall_dup = duplicate(floor_ent, position=Vec3(x_incrementer*10, -5, z_incrementer*10))
+                wall_dup = duplicate(floor_ent, position=Vec3(x_incrementer*10, -5, z_incrementer*10), parent=total)
                 wall_dup.eternal=False
                 x_incrementer += 1
                 indexer += 1
@@ -132,7 +132,11 @@ def gen_level(map_dat): #generate level from 2D list. this is where the fun begi
                     wall_dup = duplicate(floor_ent, position=Vec3(x_incrementer*10, -5, z_incrementer*10), parent=total)
                     wall_dup.eternal=False
                     wall_dup.color=color.white
+                    set_chance_height = random.randint(1,max_height)
+                    if set_chance_height >= 7:
+                        wall_dup.scale_y+=set_chance_height
                     cycles += 1
+                    set_chance_height=0
                 elif random_tile>=set_chance_wall:
                     floor_dup = duplicate(floor_ent, position=Vec3(x_incrementer*10, -10, z_incrementer*10), parent=total)
                     floor_dup.eternal=False
@@ -157,36 +161,20 @@ def gen_level(map_dat): #generate level from 2D list. this is where the fun begi
 t1=threading.Thread(target=gen_level(maplevelname))
 t1.start()
 
-def update():
-    #doom/wolfenstein style turning
-    if held_keys['left arrow']: #rotate left
-        player.rotation_y -= player_cam_speed
-    
-    if held_keys['right arrow']: #rotate right
-        player.rotation_y += player_cam_speed
 
-def renderCheck():
-    if x_len >= max_render_limit or y_len >= max_render_limit:
-        total.collider=None
-        player.gravity=0.0
+
+def update():
+    pass
 
 
 player = FirstPersonController() #base ursina first person controller
-player.position=(0,1,0) #set pos
-player.mouse_sensitivity = Vec2(0, 0) #kill the mouse
+player.position=(0,max_height+5,0) #set pos
+player.mouse_sensitivity = Vec2(0, 50) #kill the mouse
 player.cursor.color = color.red #turns the cursor gray hur dur fuck face
-player.jump_height = 6 #kills jumping
-player_cam_speed = 2 #turn speed for the camera, left and right arrow keys
-input_handler.bind('up arrow','w') #bind arrow keys forward and backward to movement, doom-style
-input_handler.bind('down arrow','s')
-input_handler.unbind('w')
-input_handler.unbind('a')
-input_handler.unbind('s')
-input_handler.unbind('d')
+player.jump_height = 5 #kills jumping
 player.eternal=True
-player.gravity = 0.50 #rate of which player character falls
+player.gravity = 0.087 #rate of which player character falls
 player.speed = 13
-
 
 total.collider='mesh'
 Entity.color="red"
@@ -194,7 +182,7 @@ Entity.color="red"
 
 scene.clear()
 
-total.texture='brick'
+total.texture='resource_structure'
 from maps.entity_data import floor_ent
 
 for z in total.children: #debug
@@ -203,14 +191,16 @@ for z in total.children: #debug
 for y in scene.entities: #debug
     print(str(y.collider) + " SCENE ENTITY")
 
-if confirmation=='y':
-    varname = random.randint(1, 9999999)
-    fullname = str("dungeon_" + str(varname))
-    total.model.save(name=fullname, folder= Path("models_compressed"))
+if maplevelname==level_data.level_data_rand_custom:
+    if confirmation=='y':
+        varname = random.randint(1, 9999999)
+        fullname = str("dungeon_" + str(varname))
+        total.model.save(name=fullname, folder= Path("models_compressed"))
 
-renderCheck()
+
 
 scene.fog_density=0.07
 scene.fog_color=color.black
+
 
 app.run() #execute the application
